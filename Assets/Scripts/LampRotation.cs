b257c59d9d1b34b59b6c85776b7264630d31dd4e
IKI-245 <kirillivanov_2009@mail.ru>
2024-11-25 20:04:49 +0300

    Disadvantage FlashLight

--- Assets/Scripts/LampRotation.cs
+++ Assets/Scripts/LampRotation.cs
@@ -6,11 +6,14 @@ public class LampRotation : MonoBehaviour
 {
     public float RotationSpeed;
     public Transform LampAxisTransform;
+    public Light flashlight;
 
     public float minAngle;
     public float maxAngle;
-    public Light flashlight;
+    
     public bool On_flashlight;
+    public float power;
+    public bool Power_Set;
 
     // Start is called before the first frame update
     void Start()
@@ -21,19 +24,27 @@ public class LampRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Power_Set == true)
         {
-            On_flashlight = !On_flashlight;
-        }
-        if (On_flashlight == true)
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                On_flashlight = !On_flashlight;
+            }
+        }  
+        if (On_flashlight == true && power > 0)
         {
+            power -= Time.deltaTime;
             flashlight.intensity = 1;
         }
         else
         {
             flashlight.intensity = 0;
         }
-
+        if (power <= 0)
+        {
+            Power_Set = false;
+            On_flashlight = false;
+        }
         transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
 
         var newAngleX = LampAxisTransform.localEulerAngles.x - +Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
@@ -42,5 +53,5 @@ public class LampRotation : MonoBehaviour
 
         newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
         LampAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
-    }
+    } 
 }
